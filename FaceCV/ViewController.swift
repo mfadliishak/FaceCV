18286e1f856309b9068eb6bc43408167da2e6764
Fadli Ishak <fadliishak@gmail.com>
2020-04-18 19:49:37 +0900

    Initial Commit

---
+++ FaceCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceCV
+//
+//  Created by Fadli Ishak on 2020/04/18.
+//  Copyright © 2020 Fadli Ishak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
